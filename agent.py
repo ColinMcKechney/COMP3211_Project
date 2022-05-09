4ef6e6b83a1f30fb6817a3fa6a5b43f32c11e3b2
Colin McKechney <cmckechn@nd.edu>
2022-05-09 20:57:50 +0800

    changed to for loop

--- agent.py
+++ agent.py
@@ -37,7 +37,9 @@ class MyAgent(BaseAgent):
             self.locations = #get locations from the database
             other_loc = self.locations.values() - self.locations[self.name]
             for j in range(len(other_loc)): #go through the other locations of the agents 
-                for k in range(len(self.locations[self.name])): #check each step
+                k=0
+                #need a while loop to pause iteration
+                while k in range(len(self.locations[self.name])): #check each step
                     if k > len(other_loc[j]): #if the other agent finishes first we don't have to check anymore
                         break
 
@@ -45,7 +47,9 @@ class MyAgent(BaseAgent):
                     if self.locations[self.name][k] == other_loc[j][k] or (self.locations[self.name][k] == other_loc[j][k-1] and self.locations[self.name][k-1] == other_loc[j][k]):
                         #collision avoidance
                         new_loc = self.collision_avoid(k)
+                        continue
                         #call algorithm to find shortest path to goal from new position             
+                    k+=1
 
         '''your code to get the next location, I'm assuming you already wrote this'''
             
