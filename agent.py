599bb77d71a079e5c3b6d4430be7e9b35e01d9ea
Colin McKechney <cmckechn@nd.edu>
2022-05-09 20:53:44 +0800

    basic outline

--- agent.py
+++ agent.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 from base import (BaseAgent, action_dict, move,
                   set_timeout, after_timeout, TIMEOUT)
 
@@ -10,6 +11,10 @@ from base import (BaseAgent, action_dict, move,
 
 
 class MyAgent(BaseAgent):
+
+    def __init__(self, name, env):
+        super().__init__(name, env)
+        self.locations = {}
 
     def get_avai_actions(self, game_state):
         avai_actions = []
@@ -28,24 +33,49 @@ class MyAgent(BaseAgent):
     @set_timeout(TIMEOUT, after_timeout)
     def get_action(self, game_state):
         # Step 1. figure out what is accessible
-        obs = self.observe(game_state)
-        avai_actions = self.get_avai_actions(game_state)
-        goal = self.env.get_goals()[self.name]
+        if self.locations == {}:
+            self.locations = #get locations from the database
+            other_loc = self.locations.values() - self.locations[self.name]
+            for j in range(len(other_loc)): #go through the other locations of the agents 
+                for k in range(len(self.locations[self.name])): #check each step
+                    if k > len(other_loc[j]): #if the other agent finishes first we don't have to check anymore
+                        break
 
-        # Step 2. production system or any rule-based system
-        min_dist = 999999
-        best_action = None
-        for action in avai_actions:
-            succ = move(obs[0], action)
-            if succ in obs[1:]:
-                continue
-            else:
-                dist = (goal[0] - succ[0]) ** 2 + (goal[1] - succ[1]) ** 2
-                if dist <= min_dist:
-                    min_dist = dist
-                    best_action = action
+                    #if then end up in the same location or if the locations swap, both would be a collision
+                    if self.locations[self.name][k] == other_loc[j][k] or (self.locations[self.name][k] == other_loc[j][k-1] and self.locations[self.name][k-1] == other_loc[j][k]):
+                        #collision avoidance
+                        new_loc = self.collision_avoid(k)
+                        #call algorithm to find shortest path to goal from new position             
 
-        # TODO: you may want to start here
-        # Feel free erase all the demo code above
+        '''your code to get the next location, I'm assuming you already wrote this'''
+            
 
         return best_action
+    
+    def collision_avoid(self, k):
+        #need: k, i, j, goals
+        game_state = {}
+        for key, item in self.locations.items():
+            game_state[key] = item[k]
+
+        actions = self.get_avai_actions(game_state)
+        try:
+            actions.remove(to_action(self.locations[self.name][k])) #to_action is a placeholder for your action function
+        except(ValueError):
+            pass
+            
+        if len(actions) > 0:
+            return self.env.transition(actions[0])[self.name] 
+        else:
+            return NULL
+
+
+
+
+    
+
+
+
+
+
+
